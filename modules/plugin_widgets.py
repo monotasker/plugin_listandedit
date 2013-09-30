b06b8c1d50dbea5be69fe11a555e63c29f0341b1
Ian W. Scott <iscott@tyndale.ca>
2013-09-30 15:40:55 -0400

    Moving classes and functions out of model file into module

--- modules/plugin_widgets.py
+++ modules/plugin_widgets.py
@@ -1,8 +1,9 @@
 # Module holding widget functions and classes for plugin_widgets
-from gluon import A, URL, SQLFORM, DIV
+from gluon import A, URL, SQLFORM, DIV, SPAN, current
+auth = current.auth
 
 
-class POPOVER():
+class POPOVER(object):
     '''
     Provides a bootstrap popover widget which allows html content.
 
@@ -85,3 +86,37 @@ class JQMODAL():
                     _href=URL(cname, fname, args=[linkargs]),
                     cid=target)
         return old_widget, modal_link
+
+
+def ICONLINK(title, text, icon):
+    linktitle = '{}_icon'.format(title)
+    link_classes = '{} icon-only icon-{}'.format(linktitle, icon)
+    link = A(SPAN(text, _class='accessible'),
+             _href='#',
+             _class=link_classes,
+             _id=linktitle)
+    return link
+
+
+def TOOLTIP(title, text, content, icon=None):
+    '''
+    Build and return a tooltip widget with the supplied content.
+    '''
+    # build widget wrapper
+    wrapper_title = '{}_wrapper'.format(title)
+    wrapper_classes = '{} tip_wrapper'.format(wrapper_title)
+    tip_wrapper = DIV(_class=wrapper_classes, _id=wrapper_title)
+
+    # add tip trigger link
+    if icon:
+        tip_wrapper.append(ICONLINK(title, text, icon))
+    else:
+        trigger_title = '{}_trigger'.format(title)
+        trigger_classes = '{} trigger'.format(trigger_title)
+        tip_wrapper.append(A(text, _classes=trigger_classes, _id=trigger_title))
+
+    # add tip content
+    tip_classes = '{} tip'.format(title)
+    tip_wrapper.append(DIV(content, _class=tip_classes, _id=title))
+
+    return tip_wrapper
