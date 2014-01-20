29f94c2db14081b185503517312624a2909f8c8d
Ian W Scott <scottianw@gmail.com>
2014-01-19 23:34:12 -0500

    Fixed some problems with bootstrap integration -- more re-styling still needed.

--- static/plugin_listandedit/plugin_listandedit.js
+++ static/plugin_listandedit/plugin_listandedit.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
 
-if($('#page').hasClass('listing')){
+if($('#main').hasClass('listing') || $('#main').hasClass('editing')){
     /* set listpane height to optimal */
     /* if pane heights are wrong, change the value of the heightadjust variable to tweak it */
-    var heightadjust = 96;
+    var heightadjust = 48;
     var lpos = $('#listpane').offset();
     var vpos = $('#viewpane').offset();
     var listtop = lpos.top;
     var viewtop = vpos.top;
     var winheight = $(window).height();
     /* I'm using the html5 footer element here, to use standard id just change 'footer' to '#footer' */
-    var headheight = $('header').height();
+    var headheight = $('#header').height();
     var navheight = $('#list_control').height();
-    var footheight = $('footer').height();
+    var footheight = $('#footer').height();
     var listheight = winheight - footheight - headheight - heightadjust - navheight;
     var viewheight = winheight - footheight - headheight - heightadjust - navheight;
     $('#listpane').css('height', listheight);
