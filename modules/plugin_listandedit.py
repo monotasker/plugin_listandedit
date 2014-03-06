6a91a0daaa0bda8ac12c6787fdc41f09513ccf6c
Ian W. Scott <iscott@tyndale.ca>
2014-03-06 13:04:12 -0500

    changes downloaded from live blog site

---
+++ modules/plugin_listandedit.py
@@ -0,0 +1,13 @@
+from gluon import LOAD
+
+# CSS imported directly into framework.less
+#response.files.append(URL('static',
+            #'plugin_listandedit/plugin_listandedit.css'))
+
+
+def plugin_listandedit():
+    '''
+    Public interface method to call in views in order to embed the
+    plugin_listandedit widget.
+    '''
+    return LOAD('plugin_listandedit', 'widget.load', ajax=True)
